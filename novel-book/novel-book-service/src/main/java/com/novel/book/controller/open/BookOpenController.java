730c3783d1ec9e66f48ada36dccd16b12591e3ac
haiyang <491680922@qq.com>
2026-06-02 18:29:39 +0800

    1

--- novel-book/novel-book-service/src/main/java/com/novel/book/controller/open/BookOpenController.java
+++ novel-book/novel-book-service/src/main/java/com/novel/book/controller/open/BookOpenController.java
@@ -1,6 +1,5 @@
 package com.novel.book.controller.open;
 
-import com.novel.book.api.BookOpenFeignApi;
 import com.novel.book.dto.BookDetailQueryDto;
 import com.novel.book.dto.BookDetailVo;
 import com.novel.book.service.BookService;
@@ -8,17 +7,20 @@ import com.novel.common.core.domain.R;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
 @RequiredArgsConstructor
-public class BookOpenController implements BookOpenFeignApi {
+@RequestMapping("/api/open/book")
+public class BookOpenController {
 
     private final BookService bookService;
 
-    @Override
+    @PostMapping("/detail")
     public R<BookDetailVo> getBookDetail(@Valid @RequestBody BookDetailQueryDto params) {
         return R.ok(bookService.getBookDetail(params).block());
     }
