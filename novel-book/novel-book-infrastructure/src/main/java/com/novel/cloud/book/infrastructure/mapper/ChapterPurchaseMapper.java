6f0855fe63abbd46c492f538e62853f00d229fdf
haiyang <491680922@qq.com>
2026-06-13 00:13:09 +0800

    1

--- novel-book/novel-book-infrastructure/src/main/java/com/novel/cloud/book/infrastructure/mapper/ChapterPurchaseMapper.java
+++
@@ -1,7 +0,0 @@
-package com.novel.cloud.book.infrastructure.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.novel.cloud.book.infrastructure.dataobject.ChapterPurchaseDO;
-
-public interface ChapterPurchaseMapper extends BaseMapper<ChapterPurchaseDO> {
-}
